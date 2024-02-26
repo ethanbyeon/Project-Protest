795a071d9fd87eff1e9a2ad1b1e14ca56ff35a51
mjlue-uc <mjlue@ucsc.edu>
2024-02-25 19:15:19 -0800

    Final Screen

--- protest_project/lib/google_apis.dart
+++ protest_project/lib/google_apis.dart
@@ -29,3 +29,70 @@ class GoogleWidget extends StatelessWidget {
     );
   }
 }
+class MapScreen extends StatefulWidget {
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  // GoogleMapController _controller;
+  List<String> _locations = List.generate(50, (index) => 'Location $index');
+  String protest_information = "Select a protest";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Map with ListView'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Row(
+            children: [
+              Expanded(
+                flex: 3,
+                child: GoogleMap(
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(37.7749, -122.4194), // San Francisco Coordinates
+                    zoom: 10,
+                  ),
+                  // onMapCreated: (controller) {
+                  //   setState(() {
+                  //     _controller = controller;
+                  //   });
+                  //},
+                ),
+              ), // Removed the extra comma here
+              Expanded(
+                flex: 2,
+                child: Text(
+                  protest_information,
+                  style: TextStyle(fontSize: 24.0),
+                ),
+              ),
+            ],
+          ),
+          ),
+          Expanded(
+            flex: 2,
+            child: ListView.builder(
+              itemCount: _locations.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_locations[index]),
+                  onTap: () {
+                    setState(() {
+                      protest_information = "New Information";
+                    });
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
